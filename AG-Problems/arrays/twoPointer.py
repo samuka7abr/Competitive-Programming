b2a1b8093564f5e2723ca3f03aeba0e9604baafa
samuka7abr <samuelabrao2006@gmail.com>
2025-02-18 17:34:46 -0300

    solved: reverse words in a string III

---
+++ AG-Problems/arrays/twoPointer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseWords(self, s):
+        res = ''
+        l, r = 0, 0
+
+        while r < len(s):
+            if s[r] != ' ':
+                r += 1
+            else: 
+                res += s[l:r+1][::-1]
+                r += 1
+                l = r
+        res += ' '
+        res += s[l:r + 2][::-1]
+
+        return res[1:]
