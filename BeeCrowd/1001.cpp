b259f3fab4ed37d5f16791817530013589d135a9
samuka7abr <samuelabrao2006@gmail.com>
2025-03-10 13:01:44 -0300

    add: beecrowd 1001

---
+++ BeeCrowd/1001.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+int main(){
+    int A, B;
+    std::cin >> A;
+    std::cin >> B;
+    
+    int sum = A + B;
+    
+    std::cout << "X = " << sum << std::endl;
+    
+    return 0;
+}
