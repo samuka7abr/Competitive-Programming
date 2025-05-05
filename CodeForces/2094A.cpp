29dde4fb7d1677128f8cdb99f135df0c158a63af
samuka7abr <samuelabrao2006@gmail.com>
2025-05-05 17:06:30 -0300

    CodeForces: 2094A

---
+++ CodeForces/2094A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        string a, b, c;
+        cin >> a >> b >> c;
+        cout << a[0] << b[0] << c[0] << endl;
+    }
+
+    return 0;
+}
