1c3dd1a32ddb4a82a7e50061d0835f902f648d2f
samuka7abr <samuelabrao2006@gmail.com>
2025-05-26 09:48:00 -0300

    LeetCode: 2

---
+++ LeetCodes/2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0), *cur = &dummy;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int v1 = l1 ? l1->val : 0;
+            int v2 = l2 ? l2->val : 0;
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            cur->next = new ListNode(sum % 10);
+            cur = cur->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        return dummy.next;
+    }
+};
