0d6a7a69a2b1f622a376e8a9f70666f8e087e822
samuka7abr <samuelabrao2006@gmail.com>
2025-06-08 13:12:14 -0300

    CodeForces: CR1029(DIV3) C

--- CodeForces/CR1029(DIV3)/03/C.cpp
+++ CodeForces/CR1029(DIV3)/03/C.cpp
@@ -4,10 +4,10 @@ using namespace std;
 #define pb push_back
 using ll = long long;
 using ull = unsigned long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
 const int INF_INT = 1e9;
-const ll INF_LL = 1e18;  
+const ll INF_LL = 1e18;
 const double PI = acos(-1.0);
 
 int main() {
@@ -17,42 +17,52 @@ int main() {
     while (T--) {
         int n;
         cin >> n;
-
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-
-        vector<int> suf(n + 1, 0);
-        for (int x : a) suf[x]++;
-
-        vector<bool> vis(n + 1, false);
-        unordered_set<int> req;
-        vector<int> cur;
-        cur.reserve(n);
-
-        int f = 0, nf = 0;
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            int x = a[i];
-            suf[x]--;
-            if (req.count(x)) {
-                req.erase(x);
-            }
-            if (!vis[x]) {
-                vis[x] = true;
-                cur.pb(x);
-                if (suf[x] > 0) f++;
-                else nf++;
-            }
-            if (req.empty() && nf == 0) {
-                res++;
-                for (int y : cur) req.insert(y);
-                for (int y : cur) vis[y] = false;
-                cur.clear();
-                f = nf = 0;
-            }
+        vector<int> a(n+1);
+        vector<int> prox(n+2);
+        vector<int> ult(n+2);
+        
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
         }
-        if (!cur.empty()) res++;
-        cout << res << '\n';
+        
+        int LIMITE = n + 1;
+        for (int i = 1; i <= n; i++) {
+            ult[i] = LIMITE;
+        }
+        
+        for (int i = n; i >= 1; i--) {
+            prox[i] = ult[a[i]];
+            ult[a[i]] = i;
+        }
+        
+        int pos = 1;
+        int count = 0;
+        
+        while (pos <= n) {
+            int fimSeg = pos;
+            while (true) {
+                int novoFim = 0;
+                
+                for (int i = pos; i <= fimSeg; i++) {
+                    if (prox[i] > novoFim) {
+                        novoFim = prox[i];
+                    }
+                }
+                
+                count++;
+                
+                if (novoFim > n) {
+                    break;
+                }
+                
+                pos = fimSeg + 1;
+                fimSeg = novoFim;
+            }
+            break;
+        }
+        
+        cout << count << endl;
     }
+    
     return 0;
 }
